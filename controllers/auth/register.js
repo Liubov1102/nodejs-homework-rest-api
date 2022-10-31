884f695e90353e6eac42b0f8cd3c560124d98b27
Liubov1102 <laav.odessa@gmail.com>
2022-10-31 13:25:26 +0200

    hw-6 start

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,20 +1,33 @@
 
 const bcrypt = require("bcryptjs")
 const gravatar = require("gravatar")
+const {nanoid} = require("nanoid")
 
 const {User} = require("../../models/user")
 
-const {RequestError} = require("../../helpers")
+const { RequestError, sendEmail } = require("../../helpers")
+
+const {BASE_URL} = process.env;
 
 const register = async(req, res)=> {
-    const { email, password} = req.body;
+    const { name, email, password} = req.body;
     const user = await User.findOne({email});
     if(user) {
         throw RequestError(409, "Email in use")
     }
-    const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
-    const result = await User.create({...req.body, password: hashPassword, avatarURL});
+    const verificationToken = nanoid();
+    const result = await User.create({ name, email, password: hashPassword, avatarURL, verificationToken });
+
+    const mail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify you email</a>`
+    };
+
+    await sendEmail(mail);
+
     res.status(201).json({
         name: result.name,
         email: result.email,
